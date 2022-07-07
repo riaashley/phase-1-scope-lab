5c7d864023ecb44750b65846b708aab83adc640d
Ria Legaspi <riaveluz@gmail.com>
2022-07-06 17:03:20 -0700

    commit tree e8556f77bd9548d960cf622dd4ee287e610a0583

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+   return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = "ria";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "joe";
+    return leastFavoriteCustomer
+}
